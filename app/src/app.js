9a8537a2ef8d33a64a8f852bbfd07cbccef8577a
Arielle Foldoe <afoldoe@gmail.com>
2016-08-16 16:49:33 -0700

    working on start room

--- app/src/app.js
+++ app/src/app.js
@@ -3,5 +3,37 @@ import angular from 'angular';
 const app = angular.module('myApp', []);
 
 app.controller('main', function($scope) {
-  $scope.hello = 'Hello World!';
+  $scope.room = '';
+  $scope.cake = false;
+  $scope.desk = `Go to your desk.`;
+  $scope.bossDesk = `Go to your Boss's desk`;
+  $scope.breakroom = `Go to the breakroom`;
+  $scope.select = function(value) {
+    $scope.room = value;
+  };
+  
+
+  
+});
+app.controller('start', function($scope) {
+  $scope.intro = `It is 4:20pm on a Friday, and your top pal has an extra ticket to the Pabst Music Festival on the Waterfront. You stuck at work until 6pm, but your pal is coming to pick you up at five. You must navigate through the perils of Coropate America to reach the safe harbor that is Pabst Music Festival. Your move!`;
+
+
+
+});
+
+app.controller('desk', function($scope) {
+
+});
+
+app.controller('bossDesk', function($scope) {
+
+});
+
+app.controller('breakroom', function($scope) {
+
+});
+
+app.controller('end', function($scope) {
+
 });
