a85ba62b88a25aac909ae36f26d03083f18f4740
Arielle Foldoe <afoldoe@gmail.com>
2016-08-18 14:23:30 -0700

    starting styling

--- app/src/controllers/main.js
+++ app/src/controllers/main.js
@@ -1,7 +1,8 @@
 export default function main($scope) {
   $scope.room = '';
   $scope.cake = false;
-  $scope.intro = `It is 4:20pm on a Friday, and your top pal has an extra ticket to the Pabst Music Festival on the Waterfront. You stuck at work until 6pm, but your pal is coming to pick you up at five. You must navigate through the perils of Coropate America to reach the safe harbor that is Pabst Music Festival. Your move!`;
+  $scope.heading = `ESCAPE FROM CORPORATE 'MURICA`;
+  $scope.intro = `It is 4:20pm on a Friday, and your top pal, Pam, has an extra ticket to the Pabst Music Festival on the Waterfront. You're stuck at work until 6pm, but Pam is coming to pick you up at five. You must navigate through the perils of Coropate America to reach the safe harbor that is Pabst Music Festival. Your boss, Michael, back from Sandals, Jamaica is heading straight towards you. Your move, Jim!`;
   $scope.getCake = function() {
     $scope.cake = true;
   };
