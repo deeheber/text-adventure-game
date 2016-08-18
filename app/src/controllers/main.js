0f4b027427728e4d775c352d1090dd6cce8dac8a
Arielle Foldoe <afoldoe@gmail.com>
2016-08-18 16:46:14 -0700

    fixed button placement

--- app/src/controllers/main.js
+++ app/src/controllers/main.js
@@ -3,7 +3,7 @@ export default function main($scope) {
   $scope.cake = false;
   $scope.heading = `ESCAPE FROM CORPORATE 'MURICA`;
   $scope.intro = `It is 4:20pm on a Friday, and your top pal, Pam, has an extra ticket to the Pabst Music Festival on the Waterfront. You're stuck at work until 6pm, but Pam is coming to pick you up at five. You must navigate through the perils of Coropate America to reach the safe harbor that is Pabst Music Festival. Your boss, Michael, back from Sandals, Jamaica is heading straight towards you. Your move, Jim!`;
-  $scope.class = `four columns`;
+  $scope.class = `four-columns`;
 
   $scope.getCake = function() {
     $scope.cake = true;
@@ -12,10 +12,22 @@ export default function main($scope) {
     $scope.room = 'end';
   };
 
-  $scope.$watch('room', function() {
+  $scope.$watch('[room, cake]', function() {
     console.log('watch works!');
-    if($scope.room === 'desk') {
-      $scope.class = `six columns`;
+    if($scope.room === `desk`) {
+      $scope.class = `six-columns`;
+    } else if ($scope.room === 'breakroom') {
+      if($scope.cake) {
+        $scope.class = `six-columns`;
+      } else {
+        $scope.class = `four-columns`;
+      }
+    } else if($scope.room === `bossDesk`) {
+      if($scope.cake) {
+        $scope.class = `four-columns`;
+      } else {
+        $scope.class = `six-columns`;
+      }
     }
-  });
+  }, true);
 }
