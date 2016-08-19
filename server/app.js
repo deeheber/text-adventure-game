14a933c3bc0b3ffb92575957ebfcb9d41a7e9eaf
Danielle H <daheber@gmail.com>
2016-08-19 16:14:00 -0700

    finished server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const app = express();
 
-// app.use( ( req, res, next ) => {
-//   const url = '*';
-//   res.header( 'Access-Control-Allow-Origin', url );
-//   res.header( 'Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE' );
-//   res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
-//   next();
-// });
-
 app.use( express.static( __dirname + '/public' ) );
 
 app.listen(3000);
