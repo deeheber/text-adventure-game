9c4496c4d68d0db0b1a3d95e80b696e9cd866a2f
Danielle H <daheber@gmail.com>
2016-08-21 08:59:12 -0700

    index.js added

--- server/app.js
+++ server/app.js
@@ -1,18 +1,4 @@
 const express = require('express');
-const app = express();
+const app = module.exports = express();
 
 app.use(express.static( __dirname + '/public' ));
-
-// app.use((req, res, next)=> {
-//   const url = '*';
-//   res.header('Access-Control-Allow-Origin', url);
-//   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-//
-// app.get('/', (req, res) => {
-//   res.send('Home Page');
-// });
-//
-// app.listen(3000);
